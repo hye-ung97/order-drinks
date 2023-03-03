5021f499758b12e587913e28d7c5c5ee0d71bd37
hye-young <hysung714@gmail.com>
2023-03-03 17:47:11 +0900

    create project

---
+++ src/main/java/com/zerobase/order_drinks/OrderDrinksApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.order_drinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderDrinksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderDrinksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zerobase/order_drinks/OrderDrinksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.order_drinks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderDrinksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
